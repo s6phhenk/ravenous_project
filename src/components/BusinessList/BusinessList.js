32bb2e24dd0aef3786ef73b3f3dec16f18e3471b
phenk53 <phenk@hs-mittweida.de>
2020-05-06 20:33:26 +0200

    Dateien noch einmal neu hochgeladen

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business';
 
  
 
-class BusinessList extends React.Component {
+class BusinessList extends Component {
     render () {
         return (
             <div className="BusinessList">
   {
       // for every business in the array, we want to render one business component
       this.props.businesses.map(business => {
-        return <Business business={business}/>
+        return <Business key={business.id} business={business}/>
       })
   };
         </div>
